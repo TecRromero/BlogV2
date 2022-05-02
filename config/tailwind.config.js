2bac273237cafeb09130bc3d2f7d15befc1b9240
Rudy Romero <TecRRomero@gmail.com>
2022-05-02 17:05:30 -0400

    add gem devise

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,10 +7,29 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
+     colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+     
     },
   },
   plugins: [
